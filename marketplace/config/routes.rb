d814b204ec4332a2ad817f3b5f0cea8401089a50
ChrisSmithArt <chrissmithart@proton.me>
2024-07-08 17:58:57 -0500

    added header and footer partials, made links to index pages for Users and Service Offers

--- marketplace/config/routes.rb
+++ marketplace/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'service_offers/index'
-  get 'service_offers/show'
-  get 'users/index'
-  get 'users/show'
+  get 'pages/index'
+  get 'pages/about'
+
+  root "pages#index"
+
+  resources :users
+  resources :service_offers
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
