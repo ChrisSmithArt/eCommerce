8e4f65f72f7561e6c0bde45bfab631ac8d4be414
ChrisSmithArt <chrissmithart@proton.me>
2024-07-10 15:03:50 -0500

    removed old breadcrumb code

--- marketplace/app/controllers/service_offers_controller.rb
+++ marketplace/app/controllers/service_offers_controller.rb
@@ -4,16 +4,10 @@
 class ServiceOffersController < ApplicationController
   def index
     @service_offers = ServiceOffer.order(:service_type_id).page(params[:page]).per(6)
-    breadcrumbs.add "Service Offers", service_offers_path
     @service_types = ServiceType.all
   end
 
   def show
     @service_offer = ServiceOffer.find(params[:id])
-    breadcrumbs.add "Service Offers", service_offers_path
-    breadcrumbs.add @service_offer.service_type.service_type_name, service_type_path
-    breadcrumbs.add @service_offer.user.user_name, user_path
-    breadcrumbs.add "#{@service_offer.user.user_name}'s #{@service_offer.service_offer_name}",
-                    service_offer_path
   end
 end
--- marketplace/app/controllers/users_controller.rb
+++ marketplace/app/controllers/users_controller.rb
@@ -4,13 +4,10 @@
 class UsersController < ApplicationController
   def index
     @users = User.order(:user_name).page(params[:page]).per(6)
-    breadcrumbs.add "Providers", users_path
     @service_types = ServiceType.all
   end
 
   def show
     @user = User.find(params[:id])
-    breadcrumbs.add "Providers", users_path
-    breadcrumbs.add "#{@user.user_name}'s Store", user_path
   end
 end
