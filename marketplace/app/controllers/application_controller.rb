48454ffe47ebdbd187ad07c3f357e48bc19cdf72
ChrisSmithArt <chrissmithart@proton.me>
2024-07-15 19:49:50 -0500

    added structure for displaying cart on a page, also added function to create a cart if no cart exists.

--- marketplace/app/controllers/application_controller.rb
+++ marketplace/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :check_cart
   helper_method :breadcrumbs
 
   def breadcrumbs
@@ -9,5 +10,15 @@ class ApplicationController < ActionController::Base
 
   def add_breadcrumb(name, path = nil)
     breadcrumbs << Breadcrumb.new(name, path)
+  end
+
+  def check_cart
+    return unless user_signed_in?
+    return if current_user.cart
+
+    Cart.create!(
+      user_id: current_user.id,
+      total:   0
+    )
   end
 end
