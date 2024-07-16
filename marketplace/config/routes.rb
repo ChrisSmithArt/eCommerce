6ffe2375f202c93c014c2b3354549805f39887ba
ChrisSmithArt <chrissmithart@proton.me>
2024-07-15 19:19:08 -0500

    added carts controller

--- marketplace/config/routes.rb
+++ marketplace/config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'carts/index'
+  get 'carts/show'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: "users/registrations"
